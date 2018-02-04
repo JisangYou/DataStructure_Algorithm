9364c7ba9ff6d7cc251fe24a6ca0f47dcbf53390
youjisang <jisangman89@gmail.com>
2018-02-05 00:26:36 +0900

    -_-

--- Algorithm_Training/Mini_Max_Sum.java
+++ Algorithm_Training/Mini_Max_Sum.java
@@ -8,24 +8,21 @@ public class Mini_Max_Sum {
 
 	static void miniMaxSum(int[] arr) {
 
-		int total = 0;
-		int max = 0;
-		int min = 0;
-
+		long total = 0;
+	
 		for (int i = 0; i < arr.length; i++) {
 			total += arr[i];
 		}
 
-		for (int j = 0; j < arr.length; j++) {
-			if (total - arr[j] > max) {
-				max = total - arr[j];
-			}
-		}
-		min = total - max;
+		Arrays.sort(arr);
 
-		System.out.print(max + " " + min);
+		System.out.print((total - arr[arr.length - 1]) + " " + (total - arr[0]));
 	}
 
+	// 문제에서 long으로 사용하라고했는데... 이를 못 읽어서 굉장히 고생함..
+	// test값들 찾아보니 원하는 데이터 중에 long이 있었음...
+	// long타입의 값들을 int로 하려다 보니 이상한 - 값들이 나왔음.
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int[] arr = new int[5];
