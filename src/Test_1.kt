281ea54e1e723b27aa5bca59196701fbee1077e0
youjisang <jisangman89@gmail.com>
2019-08-10 22:12:30 +0900

    프로퍼티까지...

---
+++ src/Test_1.kt
@@ -0,0 +1,29 @@
+import jdk.jshell.PersistentSnippet
+
+fun main(args: Array<String>) {
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello, ${name}!")
+
+    val person = Person("Bob", true)
+    println(person.name)
+    println(person.isMarried)
+}
+
+/* 자바
+
+public class Person {
+    private final String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+*/
+
+/* 코틀린 */
+
+class Person(val name: String, val isMarried: Boolean)
