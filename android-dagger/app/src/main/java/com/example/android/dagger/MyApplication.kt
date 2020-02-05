ce25128aaee76d47a7a5671926498d8de36bd68c
jisangYou <jisangman89@gmail.com>
2020-02-06 00:52:36 +0900

    provides, test etc

--- android-dagger/app/src/main/java/com/example/android/dagger/MyApplication.kt
+++ android-dagger/app/src/main/java/com/example/android/dagger/MyApplication.kt
@@ -28,13 +28,16 @@ open class MyApplication : Application() {
 //    }
 
     // Instance of the AppComponent that will be used by all the Activities in the project
+//    val appComponent: AppComponent by lazy {
+//        // Creates an instance of AppComponent using its Factory constructor
+//        // We pass the applicationContext that will be used as Context in the graph
+//        DaggerAppComponent.factory().create(applicationContext)
+//    }
     val appComponent: AppComponent by lazy {
-        // Creates an instance of AppComponent using its Factory constructor
-        // We pass the applicationContext that will be used as Context in the graph
-        DaggerAppComponent.factory().create(applicationContext)
+        initializeComponent()
     }
 
-//    open val userManager by lazy {
-//        UserManager(SharedPreferencesStorage(this))
-//    }
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
+    }
 }
