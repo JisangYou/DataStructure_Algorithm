f0ed8a2809757f3f1817c5f0b2c9c2ec816f9fb0
youjisang <jisangman89@gmail.com>
2020-03-11 21:53:03 +0900

    확장 함수 및 프로퍼티

--- KotlinInAction/src/function.kt
+++ KotlinInAction/src/function.kt
@@ -1,4 +1,6 @@
+import java.awt.Button
 import java.lang.StringBuilder
+
 
 /**
  * 코틀린 컬렉션 프레임워크 만드는 방법
@@ -15,6 +17,12 @@ fun main() {
     println(map.javaClass)
 //    println(joinToString(list,",",",",","))
     println(joinToString(list))
+    println("Kotlin".lastChar)
+    println("Kotlin".lastChar())
+    val sb = StringBuilder("Kotlin?")
+    sb.lastChar = '!'
+    println(sb)
+
 }
 
 /**
@@ -35,3 +43,44 @@ fun <T> joinToString(
     result.append(postfix)
     return result.toString()
 }
+
+/**
+ * 확장 함수 및 프로퍼티
+ */
+fun String.lastChar(): Char = this.get(this.length - 1)
+
+val String.lastChar: Char
+    get() = get(length - 1)
+
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value: Char) {
+        this.setCharAt(length - 1, value)
+    }
+
+/**
+ * 확장 함수는 오버라이드 할 수 없다.
+ */
+open class View {
+    open fun click() = println("View clicked")
+}
+
+class Button : View() {
+    override fun click() = println("Button clicked")
+}
+
+fun View.showOff() = kotlin.io.println("i am a view!")
+fun Button.showOff() = println("I'm a button!")
+
+
+
+
+
+
+
+
+
+
+
+
+
