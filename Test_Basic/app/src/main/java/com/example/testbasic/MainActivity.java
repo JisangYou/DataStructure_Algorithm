2cfc5ec279bbefad128ba89140a5ec7ecc486a9e
jisang <esse.prime.guy@gmail.com>
2021-01-11 02:01:26 +0900

    # add : basic test

---
+++ Test_Basic/app/src/main/java/com/example/testbasic/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.testbasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.testbasic.basic1.Basic1Activity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button1, button2, button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(this, Basic1Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                break;
+            case R.id.button3:
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
+
+}
---
+++ Test_Basic/app/src/test/java/com/example/testbasic/Sample.java
@@ -0,0 +1,4 @@
+package com.example.testbasic;
+
+public class Sample {
+}
