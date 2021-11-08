a082cda0dcca14334fecc9d58c99ef93f03587c2
jisang <jisangman89@gmail.com>
2021-11-09 01:03:49 +0900

    # update : [flow] flow operator

--- Coroutines-Test/src/flow.kt
+++ Coroutines-Test/src/flow.kt
@@ -1,2 +1,26 @@
-class flow {
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun simple(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking {
+    launch {
+        for (k in 1..3) {
+            println("I'm not blocked $k")
+            delay(100)
+        }
+    }
+    // Collect the flow
+    simple().collect { value -> println(value) }
+
+    (1..3).asFlow().collect { it ->
+        println(it)
+    }
 }
